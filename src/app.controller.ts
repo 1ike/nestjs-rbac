bda0f0dfa3376d88cc8df46af86d1b3b30d3f3a9
1ike <googlohren@gmail.com>
2022-01-21 23:15:45 +0300

    Add JWT auth strategy.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth/auth.service';
 
 import { LocalAuthGuard } from './auth/guards/local-auth.guard';
-
-import { User } from './users/users.service';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Controller()
 export class AppController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req): Promise<User> {
+  async login(@Request() req): Promise<any> {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
